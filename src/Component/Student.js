322f9d8a4f9cc111382e597e2c8797bd41be5610
Kavyakrishnan <kavyakrishnan9821@gmail.com>
2022-05-26 11:40:58 +0530

    complete

--- src/Component/Student.js
+++ src/Component/Student.js
@@ -1,25 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Header from './Header'
 
 const Student = () => {
+  var[name,setName]=useState("");
+  var[admissionno,setAdmissiono]=useState("");
+  var[cgpa,setCgpa]=useState("");
+  const subData=()=>{
+    const data={"name":name,"admission":admissionno,"cgpa":cgpa}
+    console.log(data)
+
+  }
   return (
-    <div><div className="Container">
+    <div>
+      <Header/>
+      <div className="Container">
     <div className="row g-2">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
             <div className="row g-3">
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">Name</label>
-                    <input type="text" className="form-control"/>
+                    <input onChange={(e)=>setName(e.target.value)} type="text" className="form-control"/>
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">Admissionno</label>
-                    <input type="text" className="form-control"/>
+                    <input onChange={(e)=>setAdmissiono(e.target.value)} type="text" className="form-control"/>
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">Cgpa</label>
-                    <input type="text" className="form-control"/>
+                    <input onChange={(e)=>setCgpa(e.target.value)} type="text" className="form-control"/>
                 </div>
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <button className="btn btn-success">Register</button>
+                    <button onClick={subData} className="btn btn-success">Register</button>
                 </div>
             </div>
         </div>
